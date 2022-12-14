56c9c81edd8e223a9d63180c7f9e67985a6a338e
Rosano <pub@xeari.com>
2022-12-14 12:28:16 +0100

    Update cosmos domain

--- banner.js
+++ banner.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	<a href="https://delta.chat">deltachat</a>
 	<a href="https://webxdc.org">webxdc</a>
 	<a href="https://bots.delta.chat">bots</a>
-	<a href="https://deltachat.github.io/cosmos/">cosmos</a>
+	<a href="https://cosmos.delta.chat">cosmos</a>
 </div>`;
 	document.body.style.paddingBottom = document.querySelector('.deltachat-banner').getBoundingClientRect().height + 'px'
 });
